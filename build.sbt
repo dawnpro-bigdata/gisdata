2a9b0f29d1d32e02f1f18ac0b80dea8d81fd56f0
changqing <changqing.ye8@gmail.com>
2018-02-26 08:28:53 +0800

    ready for a demo

--- build.sbt
+++ build.sbt
@@ -2,8 +2,35 @@ name := "evmgeo"
 
 version := "0.1"
 
-scalaVersion := "2.11.12"
+scalaVersion := "2.11.8"
 
-libraryDependencies += "org.apache.spark" % "spark-sql_2.11" % "2.1.2"
+crossScalaVersions := Seq("2.11.8")
 
-//libraryDependencies += "harsha2010" % "magellan" % "1.0.5-s_2.11"
+//sparkVersion := "2.2.0"
+
+val testSparkVersion = settingKey[String]("The version of Spark to test against.")
+
+testSparkVersion := sys.props.get("spark.testVersion").getOrElse("2.2.0")
+
+val testHadoopVersion = settingKey[String]("The version of Hadoop to test against.")
+
+testHadoopVersion := sys.props.getOrElse("hadoop.testVersion", "2.7.3")
+
+//sparkComponents := Seq("core", "sql")
+
+libraryDependencies ++= Seq(
+  "commons-io" % "commons-io" % "2.4",
+  "org.slf4j" % "slf4j-api" % "1.7.16" % "provided",
+  "com.lihaoyi" % "fastparse_2.11" % "0.4.3" % "provided",
+  "org.scalatest" %% "scalatest" % "2.2.1" % "test",
+  "com.vividsolutions" % "jts" % "1.13" % "test",
+  "com.esri.geometry" % "esri-geometry-api" % "1.2.1" % "test"
+)
+
+libraryDependencies ++= Seq(
+  "org.apache.hadoop" % "hadoop-client" % testHadoopVersion.value,
+  "org.apache.spark" %% "spark-core" % testSparkVersion.value exclude("org.apache.hadoop", "hadoop-client"),
+  "org.apache.spark" %% "spark-sql" % testSparkVersion.value  exclude("org.apache.hadoop", "hadoop-client")
+)
+
+libraryDependencies += "magellan" % "test" % "1.0" from "file:///Users/apple/Downloads/magellan-1.0.5-s_2.11.jar"
--- src/main/scala/com/dawnpro/bigdata/evmgeo/GeoTest.scala
+++ src/main/scala/com/dawnpro/bigdata/evmgeo/GeoTest.scala
@@ -2,19 +2,37 @@ package com.dawnpro.bigdata.evmgeo
 
 import org.apache.spark.sql.SparkSession
 import magellan.{Point, Polygon}
+import org.apache.spark
+import org.apache.spark.SparkConf
 import org.apache.spark.sql.magellan.dsl.expressions._
 import org.apache.spark.sql.types._
 
 object GeoTest {
   def main(args: Array[String]) {
 
+    val conf = new SparkConf()
+      .setMaster("local[2]")
+      .setAppName("MagellanUnitTest")
+      .set("spark.sql.crossJoin.enabled", "true")
+
     val spark = SparkSession.builder()
-      .appName("this is my test")
-      .master("local")
-      .config("spark.sql.warehouse.dir", "file:///Users/apple/Downloads")
+      .config(conf)
+      .config("spark.sql.crossJoin.enabled", "true")
       .getOrCreate()
-    val df = spark.sqlContext.read.format("magellan").option("type","geojson").load("mapdata")
-    df.show(10)
+    import spark.implicits._
+    val sqlContext = spark.sqlContext
+    val path = this.getClass.getClassLoader.getResource("china.json").getPath
+    val df = sqlContext.read
+      .format("magellan")
+      .option("type", "geojson")
+      .load(path)
+    //df.select($"metadata"("name") as "name").show(35)
+
+    val points = spark.createDataFrame(Seq((111.9397, 31.9263), (109.0579867, 33.9414337), (111.4228123, 34.2828126))).toDF("x","y").select(point($"x", $"y").as("point"))
+
+    val result = points.join(df.select($"polygon", $"metadata"("name").as("name"))).where($"point" within $"polygon")
+    result.show()
+
     spark.close()
   }
 }
